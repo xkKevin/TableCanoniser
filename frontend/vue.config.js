b6d9f571ec9ffd2c07f3ac424bdbb2c8f5b8663b
xkkevin <1249235131@qq.com>
2024-07-09 18:21:20 +1000

    fix warns: __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly defined

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
-    plugins: [new MonacoWebpackPlugin()],
+    plugins: [
+      new MonacoWebpackPlugin(),
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+      }),
+    ],
   },
 });
