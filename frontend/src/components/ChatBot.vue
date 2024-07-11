05d64a5282cd058bbd4f2c68845d5b245a3bc2e3
xkkevin <1249235131@qq.com>
2024-07-12 02:21:51 +1000

    add popover in ChatWithGPT

--- frontend/src/components/ChatBot.vue
+++ frontend/src/components/ChatBot.vue
@@ -1,10 +1,32 @@
 <template>
-    <div :class="['meun-switch animated flex-row', uploadflag ? 'active rubberBand off' : 'leave jello']"
-        @mouseleave="uploadleave" @mouseenter="uploadenter" v-if="uploadShow" @click.stop="logout">
-        <a-tooltip title="Chat With GPT" color="cyan" placement="left" :mouseEnterDelay="1.2" :mouseLeaveDelay="0.05">
-            <img :src="require('@/assets/robot-assistant.png')" id="chatbot" />
-        </a-tooltip>
-    </div>
+
+
+    <a-popover v-model:open="visible" trigger="click" placement="topRight" @openChange="visibleChange">
+        <template #title>
+            <div>
+                <span>Chat With GPT</span>
+                <a @click="hide" style="right: 12px; position: absolute;">Close</a>
+            </div>
+        </template>
+
+        <template #content>
+            <div class="chatbot-view">
+                <a-input-search v-model:value="chatText" placeholder="input search text">
+                    <template #enterButton>
+                        <img :src="require('@/assets/paper-plane.png')" style="height: 100%;">
+                    </template>
+                </a-input-search>
+            </div>
+        </template>
+
+        <div :class="['meun-switch animated flex-row', uploadflag ? 'active rubberBand off' : 'leave jello']"
+            @mouseleave="uploadleave" @mouseenter="uploadenter" v-if="uploadShow">
+            <a-tooltip title="Chat With GPT" color="cyan" placement="left" :mouseEnterDelay="1.2"
+                :mouseLeaveDelay="0.05">
+                <img :src="require('@/assets/robot-assistant.png')" id="chatbot" />
+            </a-tooltip>
+        </div>
+    </a-popover>
 </template>
 
 <script setup lang="ts">
@@ -12,15 +34,31 @@ import { onMounted, ref } from 'vue';
 
 const uploadflag = ref(true);
 const uploadShow = ref(false);
+let visible = ref(false);
 let chatClick = false;
+let chatText = ref('');
 
 const uploadenter = () => {
     uploadflag.value = true;
 };
 
 const uploadleave = () => {
-    if (!chatClick) uploadflag.value = false;
+    if (!chatClick) {
+        uploadflag.value = false;
+    };
 };
+
+const hide = () => {
+    // click "close" button will not trigger visibleChange event
+    visible.value = false;
+    uploadflag.value = false;
+    chatClick = false;
+};
+
+const visibleChange = (visible: boolean) => {
+    uploadflag.value = visible;
+    chatClick = visible;
+}
 
 const uploadanimated = () => {
     setTimeout(() => {
@@ -31,27 +69,62 @@ const uploadanimated = () => {
     }, 1000);
 };
 
-const logout = () => {
-    console.log('logout');
-    chatClick = true;
-    uploadflag.value = true;
-};
+// const logout = () => {
+//     chatClick = true;
+//     uploadflag.value = true;
+// };
 
 onMounted(() => {
     uploadanimated();
-    document.addEventListener('click', (event) => {
-        const target = event.target as HTMLElement;
-        if (target.id !== 'chatbot') {
-            chatClick = false;
-            uploadleave();
-        }
-    });
+    // document.addEventListener('click', (event) => {
+    //     const target = event.target as HTMLElement;
+    //     console.log(target, target.id, target.className);
+    //     if (target.id !== 'chatbot' && target.className != 'ant-popover-content') {
+    //         chatClick = false;
+    //         uploadleave();
+    //     }
+    // });
 });
 
 </script>
 
 
-<style scoped>
+<style lang="less">
+/* .chatbot-view {
+    width: 300px;
+    height: 400px;
+    overflow: auto;
+    padding: 10px;
+    right: 50px;
+    background-color: #f0f0f0;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)
+} */
+
+.ant-popover-content {
+    position: relative;
+    right: 35px;
+    // pointer-events: auto; // Set the popover content to receive events
+
+    // * {
+    //     pointer-events: none; // Set all children elements to not receive events
+    // }
+
+    .chatbot-view {
+        width: 400px;
+        height: 400px;
+        overflow: auto;
+        padding: 10px;
+        background-color: #f0f0f0;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    button {
+        padding: 4px 8px;
+    }
+}
+
 .off {
     -webkit-animation: 0.8s seconddiv;
     animation: 0.8s seconddiv;
@@ -112,7 +185,7 @@ onMounted(() => {
 
 .meun-switch {
     position: fixed;
-    bottom: 80px;
+    bottom: 60px;
     right: 0px;
     z-index: 2001;
     cursor: pointer;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,10 @@ import { createPinia } from 'pinia'
 const pinia = createPinia()
 const app = createApp(App)
 
-import { Select, Upload, Button, Tabs, Tooltip } from "ant-design-vue";
+import { Select, Upload, Button, Tabs, Tooltip, Popover, Input } from "ant-design-vue";
 // import 'ant-design-vue/dist/antd.css'
 
-app.use(pinia).use(Select).use(Tabs).use(Button).use(Upload).use(Tooltip)
+app.use(pinia).use(Select).use(Tabs).use(Button).use(Upload)
+    .use(Tooltip).use(Popover).use(Input)
 
 app.mount('#app')
